bad473da74ea6d6e5666eb6ed3df28255773b646
Alex <alexcc17@gmail.com>
2019-05-27 01:41:32 -0400

    Playing with iOS's speech and NLP features

--- Natural Language Processing/ViewController.swift
+++ Natural Language Processing/ViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import NaturalLanguage
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        speach()
+        
+        let string = "Hello, I am speaking my own lanaguage."//"私はガラスを食べられます。それは私を傷つけません。"
+        
+        let recognizer = NLLanguageRecognizer()
+        recognizer.processString(string)
+        recognizer.dominantLanguage // en // ja
+        recognizer.languageHypotheses(withMaximum: 2) // how confidence level of recognizer
+        
+        print("The langauge being spoken is: ", recognizer.dominantLanguage!, recognizer.languageHypotheses(withMaximum: 2))
         // Do any additional setup after loading the view.
+    }
+    
+    func speach(){
+//       let string = """
+//Je m'baladais sur l'avenue le cœur ouvert à l'inconnu
+//    J'avais envie de dire bonjour à n'importe qui.
+//N'importe qui et ce fut toi, je t'ai dit n'importe quoi
+//    Il suffisait de te parler, pour t'apprivoiser.
+//"""
+        let string = "This is my text. I speak like an American."
+        
+        let languageRecognizer = NLLanguageRecognizer()
+        languageRecognizer.processString(string)
+        let language = languageRecognizer.dominantLanguage!.rawValue // fr
+        
+        let speechSynthesizer = AVSpeechSynthesizer()
+        let utterance = AVSpeechUtterance(string: string)
+        utterance.voice = AVSpeechSynthesisVoice(language: language)
+        speechSynthesizer.speak(utterance)
     }
 
 
